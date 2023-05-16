be7dc4d1a0ae7093e067f664dd291c6c3e602fbe
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-05-16 14:41:23 +0100

    Fix linters errors

--- src/modules/apiRequest.js
+++ src/modules/apiRequest.js
@@ -1,8 +1,8 @@
-import { baseUrl } from "./constant";
+import { baseUrl, enpoints } from './constant.js';
 
 export const getAll = async () => {
   try {
-    const scores = await fetch(baseUrl + "/scores");
+    const scores = await fetch(baseUrl + enpoints.scores);
     const jsonFormat = await scores.json();
     return jsonFormat.result;
   } catch (error) {
@@ -12,15 +12,15 @@ export const getAll = async () => {
 
 export const createScore = async (score) => {
   try {
-    const scores = await fetch(baseUrl + "/scores", {
-        method: "POST",
-        body: JSON.stringify(score),
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      const jsonFormat = await scores.json();
-      return jsonFormat.result;
+    const scores = await fetch(baseUrl + enpoints.scores, {
+      method: 'POST',
+      body: JSON.stringify(score),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const jsonFormat = await scores.json();
+    return jsonFormat.result;
   } catch (error) {
     throw new Error(error);
   }
